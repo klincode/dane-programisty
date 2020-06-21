29832b0a61f8cdb00601dff7a5bc9d65ac2bc192
Adam Klin <adam.klin@assecobs.pl>
2020-06-21 19:08:19 +0200

    first commit

---
+++ js.js
@@ -0,0 +1,32 @@
+// Stwórz stronę zawierającą przycisk pt. "Pobierz dane"
+
+// Po kliknięciu przycisku pobierz dane o programiście w formacie JSON z internetu za pomocą Javascript lub jQuery:
+
+// https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php
+
+// Pobrane dane wyświetl poniżej przycisku w nowym, stworzonym DIVie o id="dane-programisty".
+
+// Zadanie domowe zrób za pomocą GitHub Pages.
+
+$('document').ready(function () {
+  const btn = $('<button>Pobierz dane</button>');
+  $('body').append(btn);
+  $(btn).on('click', function () {
+    $.getJSON('https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php')
+      .done(function (data) {
+        let result = '';
+        let div = '<div id="dane-programisty"></div>';
+        $(btn).after(div);
+
+        for (let key in data) {
+          result += `${key}:${data[key]} `
+        }
+        $('#dane-programisty').text(result);
+      })
+      .fail(function (error) {
+        console.log(error);
+
+      })
+  })
+
+})
